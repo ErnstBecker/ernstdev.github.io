2967f2d1a66106011aa7cb0af5ca11c31c72441a
ernstbeckerneto <ernst.becker.neto@gmail.com>
2023-05-25 08:10:27 -0300

    build: start React with react-router-dom, tailwind and postcss

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css'
+import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// * Components
+
+// ! Pages
+import HomePage from './pages/HomePage';
+
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Router>
+    {/* <Navbar/> */}
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        {/* <Route path='/*' element={<NotFoundPage />} /> */}
+      </Routes>
+    </Router>
+    </>
+  );
+};
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <p>Hello, I'm <strong className="text-[#ff0043]">Ernst</strong></p>
+      <p>FullStack</p>
+      <p>Developer</p>
+    </>
+  );
+};
+
+export default HomePage;
