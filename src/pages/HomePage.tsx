d99d3969e373fda89f35a58eb53115c269665de5
ernstbeckerneto <ernst.becker.neto@gmail.com>
2023-06-08 21:41:52 -0300

    ci: npm run build

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,9 +4,9 @@ const HomePage: React.FC = () => {
   return (
     <>
       <div className="fixed top-1/4 left-1/4 right-1/4">
-        <p className='text-5xl relative left-0'>Hello, I'm <strong className="text-[#ff0043]">Ernst</strong></p>
-        <p className='text-9xl relative'>FULL STACK</p>
-        <p className='text-9xl relative'>DEVELOPER</p>
+        <p className='text-5xl text-black relative left-0'>Hello, I'm <strong className="text-[#ff0043]">Ernst</strong></p>
+        <p className='text-9xl text-black relative'>FULL STACK</p>
+        <p className='text-9xl text-black relative'>DEVELOPER</p>
       </div>
     </>
   );
